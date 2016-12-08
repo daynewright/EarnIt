4c67ab894e27158a540e5d04ef260d28281be315
Dayne Wright <daynewr@gmail.com>
2016-12-08 13:38:11 -0600

    sample get/post requests

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,33 @@
 // Write your Javascript code.
+
+
+//sample for getting the user and logging in
+
+/*
+$.ajax({
+    headers: { 
+        'Accept': 'application/json',
+        'Content-Type': 'application/json' 
+    },
+	type : 'POST',    
+    url: "http://localhost:5000/account/loginjson",
+    data: JSON.stringify({"email" : "daynewr@gmail.com", "password" : "Ralston1$"}),
+    success: function(data) { console.log('response: ' + data) }
+});
+
+$.ajax({
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    },
+    type: 'POST',
+    url: "http://localhost:5000/account/registerjson",
+    data: JSON.stringify({"email": "daynewr@gmail.com", "password": "Ralston1$", "confirmpassword": "Ralston1$"}),
+    success: function(data) { console.log( 'response: ' + data)}
+})
+
+$.get('http://localhost:5000/account/getuser', function(data){
+   console.log(data.user);
+});
+
+*/
