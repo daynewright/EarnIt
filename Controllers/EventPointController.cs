33f792c928028e7413856cfbcbbb7f7c1b357d86
Dayne Wright <daynewr@gmail.com>
2016-12-12 10:38:37 -0600

    added get and create methods

--- Controllers/EventPointController.cs
+++ Controllers/EventPointController.cs
@@ -11,6 +11,15 @@ using System.Collections.Generic;
 
 namespace EarnIt.Controllers
 {
+    /**
+     * Class: EventPoint
+     * Purpose: Used to add an event point for the event id passed in
+     * Methods:
+     *   Task<IActionResult> All(int id) - Gets all the event points for the passed in event id if authorized
+     *       id - Event id needed to get all event points
+     *   Task<IActionResult> Create(EventPoint eventPoint) - Creates an event point for the event id
+     *       eventPoint - A json object with the information to add the event point to the database
+     **/
     [Produces("application/json")]
     public class EventPointController : Controller
     {
@@ -24,23 +33,49 @@ namespace EarnIt.Controllers
             context = ctx;
         }
 
+        /**
+         * Purpose: Gets all the event points for the passed in event id
+         * Arguments:
+         *      id - The event id for the points needed
+         * Return:
+         *      If successful then an object with the event points for the id otherwise a custom bad request object
+         */
         [HttpGet]
         [Authorize]
-        public async Task<IActionResult> GetAll([FromRoute] int id)
+        public async Task<IActionResult> All([FromRoute] int id)
         {
             List<EventPoint> EventPoints = new List<EventPoint>();
 
             EventPoints = await context.EventPoint.Where(ep => ep.EventId == id).ToListAsync();
 
-            return Json( new { success = "Event Points found!", EventPoints});
+            if(EventPoints.Any())
+            {
+                return Json( new { success = "Event Points found!", EventPoints});
+            }
 
+            return BadRequest(new { error = $"Unable to find any points for event #{id}" });
         }
 
+       /**
+         * Purpose: Creates a new event point for the event id passed in the model if authorized
+         * Arguments:
+         *      eventPoint - An object containing what is needed to post the event point
+         * Return:
+         *      If successful then an object with the created event point otherwise a custom bad request object
+         */
         [HttpPost]
         [Authorize]
-        public async Task<IActionResult> Get([FromRoute] int id)
+        public async Task<IActionResult> Create([FromBody] EventPoint eventPoint)
         {
-            
+            if(ModelState.IsValid)
+            {
+                context.Add(eventPoint);
+                await context.SaveChangesAsync();
+                await context.Entry(eventPoint).GetDatabaseValuesAsync();
+                return Json( new {success = "Event point returned!", eventPoint});
+            }
+
+            return BadRequest(new { error = "Unable to save the event point", eventPoint });
         }
     }
 }
